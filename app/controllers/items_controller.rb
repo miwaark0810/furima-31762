9c198c931874133ca214d22bdad1c3aa56ff3b8b
miwaark0810 <a.niwa80@outlook.jp>
2020-11-16 14:23:08 +0900

    itemsコントローラー内における共通の処理を整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless @item.user_id == current_user.id
   end
 
@@ -43,5 +42,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :name, :text, :price, :image, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_time_id
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
