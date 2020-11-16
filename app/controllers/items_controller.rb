d802c17a86480e4a27f6add048ad5994473332d2
miwaark0810 <a.niwa80@outlook.jp>
2020-11-16 13:00:25 +0900

    updateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -27,6 +27,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+     redirect_to item_path
+    else
+     render :edit
     end
   end
 
