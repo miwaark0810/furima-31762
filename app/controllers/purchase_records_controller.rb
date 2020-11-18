e4dcc67b38721adc2e9c414ccd193892672b928f
miwaark0810 <a.niwa80@outlook.jp>
2020-11-18 15:43:15 +0900

    購入記録の保存機能実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,24 @@
 class PurchaseRecordsController < ApplicationController
   def index
     @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+      if @purchase_address.valid?
+        @purchase_address.save
+        redirect_to root_path
+      else
+        @item = Item.find(params[:item_id])
+        render action: :index
+      end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_record_id
 
   with_options presence: true do
-    validates :token
-    validates :postal_code, numericality: {with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
     validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
     validates :city
     validates :address
@@ -12,6 +11,7 @@ class PurchaseAddress
   end
 
   def save
-    ShippngAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number)
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
